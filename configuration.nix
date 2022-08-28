ee2f7267cc9e88db60c5cd066ab9406c122cff8f
Hunter Thueson <hunter.thueson@gmail.com>
2022-08-28 00:49:17 -0600

    docs: remove unused commented-out default features and configurations

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,10 @@
         extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
         loader = {
+            efi = {
+                canTouchEfiVariables = true;
+                efiSysMountPoint = "/boot/efi";
+            };
             grub = {								# Use the GRUB 2 boot loader
                 enable = true;
                 device = "nodev";
@@ -31,50 +35,40 @@
                 version = 2;
                 useOSProber = true;
             };
-            efi = {
-                canTouchEfiVariables = true;
-                efiSysMountPoint = "/boot/efi";
-            };
         };
     };
-
-    networking.networkmanager.enable = true;					# Enables networking via NetworkManager
-    networking.hostName = "nixos"; 						# (note: NetworkManager & wpa_supplicant are not compatible)
 
     time = {
         timeZone = "America/Denver";						# Set your time zone.
         hardwareClockInLocalTime = true;					# Keep the hardware clock in local time instead of UTC
     };										# for compatibility with Windows Dual Boot
 
+    networking.networkmanager.enable = true;					# Enables networking via NetworkManager
+    networking.hostName = "the-glass-tower"; 					# (note: NetworkManager & wpa_supplicant are not compatible)
+
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp4s0f1.useDHCP = true;
-  networking.interfaces.wlp3s0.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    networking.useDHCP = false;
+    networking.interfaces.enp4s0f1.useDHCP = true;
+    networking.interfaces.wlp3s0.useDHCP = true;
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+        font = "Lat2-Terminus16";
+        keyMap = "us";
+    };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+    services.xserver.enable = true;
 
   # Enable proprietary NVIDIA drivers
     environment.systemPackages = with pkgs; [
         pciutils
         file
-        
         gnumake
         gcc
-        
         cudatoolkit
     ]; 
     nixpkgs.config.allowUnfree = true;
@@ -106,58 +100,25 @@
         Option		"metamodes" "nvidia-auto-select +0+0 {ForceCompositionPipeline=On}"
     '';
 
-  # Enable and configure lightdm
-  #  services.xserver.displayManager.lightdm = {
-  #   enable = true;
-  #  greeters.gtk.clock-format = "%F"; # Equivalent to "%Y-%m-%d"
-
-    # Force users to enter their username as well as password at login
-    # extraSeatDefaults = ''
-       #  greeter-show-manual-login=true
-        #greeter-hide-users = true
-        #allow-guest = false
-      #'';
-
-  #};
-
   # Enable SDDM
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.sddm.autoNumlock = true;
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.displayManager.sddm.autoNumlock = true;
 
   # Enable the Plasma 5 Desktop Environment.
-  services.xserver.desktopManager.plasma5.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
   
   # Configure keymap in X11
-  services.xserver.layout = "us";
+    services.xserver.layout = "us";
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+    services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktop managers).
-  services.xserver.libinput.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    services.xserver.libinput.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -165,7 +126,8 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
+
+    system.stateVersion = "21.11"; # Did you read the comment?
 
 }
 
