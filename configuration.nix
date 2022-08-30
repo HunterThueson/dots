52522148f503de0d5459242b9ac56e9ef0fb4432
Hunter Thueson <hunter.thueson@gmail.com>
2022-08-30 16:16:13 -0600

    refactor: rewrite networking configuration to be more readable and less verbose

--- configuration.nix
+++ configuration.nix
@@ -44,15 +44,17 @@
         hardwareClockInLocalTime = true;					# Keep the hardware clock in local time instead of UTC
     };										# for compatibility with Windows Dual Boot
 
-    networking.networkmanager.enable = true;					# Enables networking via NetworkManager
-    networking.hostName = "the-glass-tower"; 					# (note: NetworkManager & wpa_supplicant are not compatible)
+    networking = {
+        networkmanager.enable = true;						# Enables networking via NetworkManager
+        hostName = "the-glass-tower";						# (note: NetworkManager & wpa_supplicant are not compatible)
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-    networking.useDHCP = false;
-    networking.interfaces.enp4s0f1.useDHCP = true;
-    networking.interfaces.wlp3s0.useDHCP = true;
+        useDHCP = false;							# The global useDHCP flag is deprecated, therefore explicitly
+										# set to false here. Per-interface useDHCP will be mandatory
+        interfaces = {								# in the future, so this generated config replicates the
+            enp4s0f1.useDHCP = true;						# default behaviour.
+            wlp3s0.useDHCP = true;
+        };
+    };
 
   # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
