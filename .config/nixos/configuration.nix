6d9c2c3d28592dc7f3f8a5b18a8ee8bbe1f43a13
Hunter Thueson <hunter.thueson@gmail.com>
2022-09-18 13:53:00 -0600

    Revert "ref(gpu): separate nvidia logic"
    This reverts commit 90a73025f4a8f2d26a019fe49c319d72a16db8ba.

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -9,12 +9,20 @@
             ./hardware-configuration.nix					# include the results of the hardware scan
             ./users.nix								# for user definitions
             ./packages.nix							# for package management
-            ./nvidia.nix
         ];
 
     boot = {
 
+        kernelParams =
+            [
+                "acpi_rev_override"
+                "mem_sleep_default=deep"
+                "intel_iommu=igfx_off"
+                "nvidia-drm.modeset=1"
+            ];
+
         kernelPackages = pkgs.linuxPackages_latest;				# Use the latest LTS Linux kernel
+        extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
         loader = {
             efi = {
@@ -69,6 +77,43 @@
 
         desktopManager.plasma5.enable = true;	# Enable the KDE Plasma 5 desktop environment
     };
+
+  # Enable proprietary NVIDIA drivers
+    services.xserver.videoDrivers = [ "nvidia" ];
+    nixpkgs.config.allowUnfree = true;
+    environment.systemPackages = with pkgs; [
+        pciutils
+        file
+        gnumake
+        gcc
+        cudatoolkit
+    ]; 
+    hardware.opengl = {
+        enable = true;
+        driSupport = true;
+        extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
+        driSupport32Bit = true;
+        extraPackages32 = with pkgs.pkgsi686Linux; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
+    };
+    hardware.nvidia = {
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        modesetting.enable = true;						# Should fix screen tearing w/ Optimus via PRIME
+        prime = {
+            intelBusId = "PCI:0:2:0";
+            nvidiaBusId = "PCI:1:0:0";
+            sync.enable = true;
+            sync.allowExternalGpu = true;
+        };
+    };
+    systemd.services.nvidia-control-devices = {
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig.ExecStart = "${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi";
+    };
+
+  # Fix screen tearing with Nvidia proprietary drivers
+    services.xserver.screenSection = ''
+        Option		"metamodes" "nvidia-auto-select +0+0 {ForceCompositionPipeline=On}"
+    '';
 
   # Enable CUPS to print documents.
     services.printing.enable = true;
