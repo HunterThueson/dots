c3e77a4e16e8f48f621d582a957f1cc4635c4cf7
Hunter Thueson <hunter.thueson@gmail.com>
2022-09-28 02:35:37 -0600

    feat(bootloader): # of GRUB menu entries 5 -> 10

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -35,7 +35,7 @@
                 efiSupport = true;
                 version = 2;
                 useOSProber = true;
-                configurationLimit = 5;                                         # Limit the number of GRUB menu entries
+                configurationLimit = 10;                                        # Limit the number of GRUB menu entries
             };
         };
     };
