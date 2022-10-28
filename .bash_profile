b5dfa32d43e6e60efd11cdfeea1b22617e73a430
Hunter Thueson <hunter.thueson@gmail.com>
2022-10-28 09:26:30 -0600

    feat(bash): configure `bash` via `home-manager`
    Switch from the old-fashioned `~/.bash_profile` + `~/.bashrc` etc.
    method of configuring `bash` to the new and improved Nix method (using
    `home-manager`). Makes things more portable and easier to redeploy if
    the need arises.
    
    Fixes #7, Fixes #8, Fixes #23

--- .bash_profile
+++ .bash_profile
@@ -1,15 +1,1 @@
-#!/usr/bin/env bash
-
-# ~/.bash_profile
-
-[[ -f ~/.config/bash/bashrc ]] && . ~/.config/bash/bashrc
-
-export PATH=$HOME/.cargo/bin:$PATH:$HOME/bin
-
-# Set XDG base directories
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-
-# Explicitly declare location of config files (even if not strictly necessary)
-export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship.toml"
-export STARSHIP_CACHE="$XDG_CACHE_HOME/starship"
+/nix/store/w9711p54f4689jvs8wrrgclqi8ghmalk-home-manager-files/.bash_profile
--- .config/nixos/users/hunter.nix
+++ .config/nixos/users/hunter.nix
@@ -5,25 +5,126 @@
 
 {
 
+# Extra directories to add to PATH
+home.sessionPath = [
+    "$HOME/.cargo/bin"
+    "$HOME/bin/"
+    "$HOME/bin/nail-clipper"
+    "$HOME/lib/bash-tome"
+];
+
+###########################
+## Package Configuration ##
+###########################
+
 nixpkgs.config.allowUnfree = true;
 
 home.packages = with pkgs; [
-    firefox                 # web browser
-    spotify                 # music
-    mailspring              # email client
-    runelite                # game for masochists
-    discord                 # chat client
-    kate                    # KDE graphical text editor
-    calibre                 # ebook client
-    gimp                    # GNU Image Manipulation Program
-    cmatrix                 # look like freakin' HACKERMAN (so powerful he could hack time itself)
-    neo-cowsay              # `cowsay` rewritten in Go (with bonus features!)
-    mpv                     # video playback
-    ffmpeg                  # video transcoding utility (and a dependency for many other programs)
-    imagemagick             # image editing tools for the command line
-    yt-dlp                  # `youtube-dl` fork; download videos from websites like YouTube
-    speedcrunch             # calculator
-    unclutter-xfixes        # auto-hide cursor with `unclutter` (but better!)
+    firefox                     # web browser
+    spotify                     # music
+    mailspring                  # email client
+    runelite                    # game for masochists
+    discord                     # chat client
+    kate                        # KDE graphical text editor
+    calibre                     # ebook client
+    gimp                        # GNU Image Manipulation Program
+    cmatrix                     # look like freakin' HACKERMAN (so powerful he could hack time itself)
+    neo-cowsay                  # `cowsay` rewritten in Go (with bonus features!)
+    mpv                         # video playback
+    ffmpeg                      # video transcoding utility (and a dependency for many other programs)
+    imagemagick                 # image editing tools for the command line
+    yt-dlp                      # `youtube-dl` fork; download videos from websites like YouTube
+    speedcrunch                 # calculator
+    unclutter-xfixes            # auto-hide cursor with `unclutter` (but better!)
 ];
+
+########################
+## Bash Configuration ##
+########################
+
+programs.bash = {
+    enable = true;
+    historyFile = "$HOME/docs/.bash_history";         # Location of the bash history file
+    historyIgnore = [                                 # List of commands that should not be saved to the history list
+        "ls"
+        "exa"
+        "cd"
+        "exit"
+        "clear"
+    ];
+    sessionVariables = {                              # Environment variables that will be set for the Bash session
+        XDG_CONFIG_HOME = "$HOME/.config";
+        XDG_CACHE_HOME = "$HOME/.cache";
+        STARSHIP_CONFIG = "$XDG_CONFIG_HOME/starship.toml";
+        STARSHIP_CACHE = "$XDG_CACHE_HOME/starship";
+    };
+
+    ###############
+    ## ~/.bashrc ##
+    ###############
+
+    bashrcExtra = ''
+        # Enable Starship prompt
+        eval "$(starship init bash)"
+
+        # Teleport to $HOME
+        cdd () {
+            cd $HOME
+            clear
+            sleep 0.01
+            exa -xDG
+        }
+
+        # Teleport to ~/.config
+        cdc () {
+            cd $HOME/.config
+            clear
+            exa -D
+        }
+
+        # Teleport to NixOS configuration directory
+        cdn () {
+            cd $HOME/.config/nixos/
+            clear
+            exa
+        }
+    '';
+
+    ###################
+    ## Shell Options ##
+    ###################
+
+    shellOptions = [
+        # Append to history file rather than replacing it
+        "histappend"
+
+        # Check the window size after each command and, if necessary, update the values of LINES and COLUMNS.
+        "checkwinsize"
+
+        # Extended globbing
+        "extglob"
+        "globstar"
+
+        # Warn if closing shell with running jobs
+        "checkjobs"
+    ];
+
+    #####################
+    ## ~/.bash_aliases ##
+    #####################
+    shellAliases = {
+        # Drop-in program replacements
+        ls = "exa -G --color=always --icons";                                # replace `ls` with `exa` (faster, written in Rust)
+        find = "fd";                                                         # replace `find` with `fd` (faster, written in -- you guessed it -- Rust)
+        
+        # Navigation
+        lsa = "exa -Gau --git --time-style long-iso --color=always --icons"; # more info about hidden files (w/ git status)
+        lsd = "exa -D --color=always --icons";                               # list only directories
+        
+        # Terminal clearing
+        cl = "clear";                                                        # because typing `clear` just takes too long
+    };
+
+};
 
 }
