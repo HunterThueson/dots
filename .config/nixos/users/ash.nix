a06184a1efeafeeea2c9751f3844288e3301f664
Hunter Thueson <hunter.thueson@gmail.com>
2022-11-11 04:06:13 -0700

    feat(shell): add QoL shell changes to alt user acc
    copy automatic bash configuration from hunter.nix to ash.nix

--- .config/nixos/users/ash.nix
+++ .config/nixos/users/ash.nix
@@ -21,6 +21,95 @@ home.packages = with pkgs; [
     speedcrunch             # calculator
 ];
 
+########################
+## Bash Configuration ##
+########################
+
+programs.bash = {
+    enable = true;
+    historyFile = "$HOME/docs/.bash_history";         # Location of the bash history file
+    historyIgnore = [                                 # List of commands that should not be saved to the history list
+        "ls"
+        "exa"
+        "cd"
+        "exit"
+        "clear"
+    ];
+    sessionVariables = {                              # Environment variables that will be set for the Bash session
+        XDG_CONFIG_HOME = "$HOME/.config";
+        XDG_CACHE_HOME = "$HOME/.cache";
+        STARSHIP_CONFIG = "$XDG_CONFIG_HOME/starship.toml";
+        STARSHIP_CACHE = "$XDG_CACHE_HOME/starship";
+    };
+
+    ###############
+    ## ~/.bashrc ##
+    ###############
+
+    bashrcExtra = ''
+        # Enable Starship prompt
+        eval "$(starship init bash)"
+
+        # Teleport to $HOME
+        cdd () {
+            cd $HOME
+            clear
+            sleep 0.01
+            exa -xDG --icons
+        }
+
+        # Teleport to ~/.config
+        cdc () {
+            cd $HOME/.config
+            clear
+            exa -D --icons
+        }
+
+        # Teleport to NixOS configuration directory
+        cdn () {
+            cd $HOME/.config/nixos/
+            clear
+            exa --icons
+        }
+    '';
+
+    ###################
+    ## Shell Options ##
+    ###################
+
+    shellOptions = [
+        # Append to history file rather than replacing it
+        "histappend"
+
+        # Check the window size after each command and, if necessary, update the values of LINES and COLUMNS.
+        "checkwinsize"
+
+        # Extended globbing
+        "extglob"
+        "globstar"
+
+        # Warn if closing shell with running jobs
+        "checkjobs"
+    ];
+
+    #####################
+    ## ~/.bash_aliases ##
+    #####################
+    shellAliases = {
+        # Drop-in program replacements
+        ls = "exa -G --color=always --icons";                                # replace `ls` with `exa` (faster, written in Rust)
+        find = "fd";                                                         # replace `find` with `fd` (faster, written in -- you guessed it -- Rust)
+        
+        # Navigation
+        lsa = "exa -Gau --git --time-style long-iso --color=always --icons"; # more info about hidden files (w/ git status)
+        lsd = "exa -D --color=always --icons";                               # list only directories
+        
+        # Terminal clearing
+        cl = "clear";                                                        # because typing `clear` just takes too long
+    };
+
+};
+
 ################
 ##  Services  ##
 ################
