aa97bd01cbb78aaf4355f2a94c1885985c791f00
Hunter Thueson <hunter.thueson@gmail.com>
2022-11-12 21:56:37 -0700

    refactor(hardware): use EDID instead of DFP values
    switch from DFP values (which can change based on which GPU port the
    monitor is connected to) to persistent hardware-based EDID values (which
    will always be the same for a given monitor). This may cause issues down
    the line if I ever have a system with two monitors with the same EDID,
    but that's unlikely and is a non-issue at present.

--- .config/nixos/xorg.nix
+++ .config/nixos/xorg.nix
@@ -25,7 +25,7 @@
         '';
 
         screenSection = ''
-            Option        "MetaModes" "DFP-1: 3840x2160_144 @3840x2160 +2560+0 {ViewPortIn=3840x2160, ViewPortOut=3840x2160+0+0, ForceCompositionPipeline=On, AllowGSYNCCompatible=On}, DFP-3: 2560x1440_144 @2560x1440 +0+360 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0, ForceCompositionPipeline=On, AllowGSYNC=On}"
+            Option        "MetaModes" "DPY-EDID-809ecabe-c3d2-29e6-1a2c-7adf94323603: 3840x2160_144 @3840x2160 +2560+0 {ViewPortIn=3840x2160, ViewPortOut=3840x2160+0+0, ForceCompositionPipeline=On, AllowGSYNCCompatible=On}, DPY-EDID-bf730b70-03cb-6513-f349-55323aee38c4: 2560x1440_144 @2560x1440 +0+360 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0, ForceCompositionPipeline=On, AllowGSYNC=On}"
             Option        "SLI" "off"
             Option        "MultiGPU" "off"
             Option        "nvidiaXineramaInfo" "false"
@@ -49,3 +49,9 @@
         '';
     };
 }
+
+# Gigabyte M28U 4k monitor EDID:
+    # DPY-EDID-809ecabe-c3d2-29e6-1a2c-7adf94323603
+
+# Dell Ultrasharp S2417DG 1440p monitor EDID:
+    # DPY-EDID-bf730b70-03cb-6513-f349-55323aee38c4
