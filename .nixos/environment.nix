d198b2c73f27badf8686976f57a344b3aa737a7a
Hunter Thueson <hunter.thueson@gmail.com>
2023-02-04 11:25:34 -0700

    feat(`vim`): highlight column 80 if necessary
    Fixes #45

--- .nixos/environment.nix
+++ .nixos/environment.nix
@@ -129,34 +129,37 @@
             ###############
     
             customRC = ''
-                set nocompatible                    " Disable vi compatibility mode as it can sometimes cause issues
-                set backspace=indent,eol,start      " Ensures that backspace behavior will function as expected
+                set nocompatible                            " Disable vi compatibility mode as it can sometimes cause issues
+                set backspace=indent,eol,start              " Ensures that backspace behavior will function as expected
                 
                 " Filetype Options "
-                syntax on                           " Turn on syntax highlighting for files with a shebang
-                filetype on                         " Enable file type detection
-                filetype plugin on                  " Enable plugins and load plugin for the detected file type
-                filetype indent on                  " Load an indent file for the detected file type
+                syntax on                                   " Turn on syntax highlighting for files with a shebang
+                filetype on                                 " Enable file type detection
+                filetype plugin on                          " Enable plugins and load plugin for the detected file type
+                filetype indent on                          " Load an indent file for the detected file type
                 
                 " Appearance Options "
-                set number relativenumber           " Use hybrid line numbers on the left side of the screen
-                set nowrap                          " Do not wrap; allow long lines to extend as far as needed
-                set ruler                           " Show current position of cursor in bottom right corner
+                set number relativenumber                   " Use hybrid line numbers on the left side of the screen
+                set nowrap                                  " Do not wrap; allow long lines to extend as far as needed
+                set ruler                                   " Show current position of cursor in bottom right corner
+
+                highlight ColorColumn ctermbg=magenta       " Highlight the 81st column of every line
+                call matchadd('ColorColumn', '\%81v', 100)  " ...but only if the line actually extends to line 81
                 
                 " Search & History Options "
-                set incsearch                       " Start highlighting results as soon as you start typing
-                set hlsearch                        " Highlight results when searching
-                set history=700                     " Remember much further into the past
+                set incsearch                               " Start highlighting results as soon as you start typing
+                set hlsearch                                " Highlight results when searching
+                set history=700                             " Remember much further into the past
                 
                 " Formatting Options "
-                set autoindent                      " Automatically indent new lines to match formatting
-                set expandtab                       " Convert tabs to corresponding number of spaces
-                set tabstop=4                       " Set width of tabs by number of columns
-                set shiftwidth=4                    " Manage indentation width when using `>>` or `<<`
+                set autoindent                              " Automatically indent new lines to match formatting
+                set expandtab                               " Convert tabs to corresponding number of spaces
+                set tabstop=4                               " Set width of tabs by number of columns
+                set shiftwidth=4                            " Manage indentation width when using `>>` or `<<`
     
                 " System Integration Options
-                set clipboard+=unnamedplus          " Always use the clipboard for ALL operations (instead of
-                                                    " interacting with the '+' and '*' registers explicitly)
+                set clipboard+=unnamedplus                  " Always use the clipboard for ALL operations (instead of
+                                                            " interacting with the '+' and '*' registers explicitly)
             '';
         };
     })
