31bc3bcd529f152d6db9be9de59bd31d797358da
Hunter Thueson <hunter.thueson@gmail.com>
2023-03-05 07:43:39 -0700

    feat: add `./modules/template.nix`
    Template for easy creation of future modules

---
+++ .nixos/modules/template.nix
@@ -0,0 +1,17 @@
+# ./modules/template.nix
+
+{
+
+  imports = [
+    # paths to other modules
+  ];
+
+  options = [
+    # option declarations
+  ];
+
+  config = [
+    # option definitions
+  ];
+
+}
