662f5f3a2a2f892ac789829374f33f73c8fd1b1a
Hunter Thueson <hunter.thueson@gmail.com>
2023-04-15 00:01:59 -0600

    fix(`minor`): change tab width to 2 spaces

--- .nixos/flake.nix
+++ .nixos/flake.nix
@@ -9,60 +9,60 @@
 #
 
 {
-    description = "Hunter Thueson's NixOS System Configuration";
+  description = "Hunter Thueson's NixOS System Configuration";
 
 ############
 #  Inputs  #
 ############
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-        home-manager = {
-            url = "github:nix-community/home-manager/release-22.11";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
 #############
 #  Outputs  #
 #############
 
-    outputs = inputs @ { self, nixpkgs, home-manager, ... }:
+  outputs = inputs @ { self, nixpkgs, home-manager, ... }:
 
-    let
-        system = "x86_64-linux";
+  let
+    system = "x86_64-linux";
 
-        pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;      # Enable proprietary software
-            overlays = [];
-        };
-
-        inherit (nixpkgs) lib;
-    in
-
-    {
-        nixosConfigurations = {
-            the-glass-tower = lib.nixosSystem {
-                inherit system;
-                modules = [
-                    (import ./configuration.nix inputs)
-                    home-manager.nixosModules.home-manager {
-
-                        home-manager = {
-                            useGlobalPkgs = true;
-                            useUserPackages = true;
-
-                            # User configuration(s)
-                            users = {
-                                hunter = import ./users/hunter.nix;
-                                ash = import ./users/ash.nix;
-                            };
-                        };
-
-                    }
-                ];
-            };
-        };
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;      # Enable proprietary software
+      overlays = [];
     };
+
+    inherit (nixpkgs) lib;
+  in
+
+  {
+    nixosConfigurations = {
+      the-glass-tower = lib.nixosSystem {
+        inherit system;
+        modules = [
+          (import ./configuration.nix inputs)
+
+          home-manager.nixosModules.home-manager {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+
+              # User configuration(s)
+              users = {
+                hunter = import ./users/hunter.nix;
+                ash = import ./users/ash.nix;
+              };
+            };
+          }
+
+        ];
+      };
+    };
+  };
 }
