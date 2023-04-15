625e01acf8b734d3e3234a37f4fdc835cf45cdc0
Hunter Thueson <hunter.thueson@gmail.com>
2023-04-15 00:55:35 -0600

    feat(`emacs`): enable `evil` & `evil-collection`
    finally, `emacs` is as usable as `vim`!

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -175,3 +175,29 @@ Exempt major modes are defined in `display-line-numbers-exempt-modes'."
 (general-define-key
   "C-M-j" 'counsel-switch-buffer)           ;; Switch buffers w/ `Ctrl+Alt+j`
 
+;;-------------;;
+;;  Evil Mode  ;;
+;;-------------;;
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
