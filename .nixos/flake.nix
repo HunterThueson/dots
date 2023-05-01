d8e94cdc02314790efe72dc89f07798867e83e3d
Hunter Thueson <hunter.thueson@gmail.com>
2023-05-01 00:04:02 -0600

    rework(`users`): temporary/untested flake rewrite
    this commit will be overwritten/amended in the near future when I
    continue the flake.nix user management rework project. As of now, the
    changes in this commit have not been tested and are unlikely to work
    properly. The only reason I'm committing these changes at all is because
    I need to run `nixos-rebuild` with an unmodified `flake.nix` and I
    didn't want to lose the progress I've made so far.

--- .nixos/flake.nix
+++ .nixos/flake.nix
@@ -30,7 +30,7 @@
   outputs = inputs @ { self, nixpkgs, home-manager, ... }:
 
   let
-    system = "x86_64-linux";
+    inherit (nixpkgs) lib;
 
     pkgs = import nixpkgs {
       inherit system;
@@ -38,31 +38,50 @@
       overlays = [];
     };
 
-    inherit (nixpkgs) lib;
+    util = import ./lib {
+      inherit system pkgs home-manager lib; overlays = (pkgs.overlays);
+    };
+
+    inherit (util) user;
+    inherit (util) host;
+
+    system = "x86_64-linux";
   in
 
   {
-    nixosConfigurations = {
-      the-glass-tower = lib.nixosSystem {
-        inherit system;
-        modules = [
-          (import ./configuration.nix inputs)
-
-          home-manager.nixosModules.home-manager {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-
-              # User configuration(s)
-              users = {
-                hunter = import ./users/hunter.nix;
-                ash = import ./users/ash.nix;
-              };
-            };
-          }
-
-        ];
+      homeManagerConfigurations = {
+          hunter = user.mkUser {
+              username = "hunter";
+              description = "Hunter";
+              groups = [ "wheel" "video" "networkmanager" "wizard" ];
+              uid = 1000;
+              shell = pkgs.bash;
+              hashedPassword = "$6$rounds=500000$ilzR8OoFwfvEOzfO$iJ9QJzjIINDW8ON33jTIIxe/B2XcB3MnCR7/qaA6NC2Sw6efZvX2HJ4l3vif8/ggmAv/4GutT8Xt4/wAgLW0H.";
+          };
       };
-    };
+
+      nixosConfigurations = {
+        the-glass-tower = lib.nixosSystem {
+          inherit system;
+          modules = [
+            (import ./configuration.nix inputs)
+
+            home-manager.nixosModules.home-manager {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+
+                # User configuration(s)
+                users = {
+                  hunter = import ./users/hunter.nix;
+                  ash = import ./users/ash.nix;
+                };
+              };
+            }
+
+          ];
+        };
+      };
+
   };
 }
